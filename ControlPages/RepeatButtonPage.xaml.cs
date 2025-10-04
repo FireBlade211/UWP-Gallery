599febeeff891f50c9aa2aa984ade1ccf34e21f5
SpectralBlade <megajustdancer@gmail.com>
2025-10-04 09:14:52 +0200

    Added RepeatButton page

---
+++ ControlPages/RepeatButtonPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWPGallery.ControlPages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class RepeatButtonPage : Page
+    {
+        private int ClickCount = 0;
+
+        public RepeatButtonPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClickCount++;
+
+            clickCountBlock.Text = $"Click count: {ClickCount}";
+        }
+    }
+}
