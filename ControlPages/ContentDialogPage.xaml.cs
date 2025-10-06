7cb0b84de432e245457e60d8b57077b2fa332b3d
SpectralBlade <megajustdancer@gmail.com>
2025-10-06 21:11:35 +0200

    Add ContentDialog samples

---
+++ ControlPages/ContentDialogPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWPGallery.ControlPages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ContentDialogPage : Page
+    {
+        public ContentDialogPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new ContentDialog
+            {
+                Title = "Unsaved changes",
+                Content = "There are unsaved changes in the document Untitled. Do you want to save changes?",
+                PrimaryButtonText = "Save",
+                SecondaryButtonText = "Don't save",
+                CloseButtonText = "Cancel",
+                DefaultButton = ContentDialogButton.Primary
+            };
+
+            var button = await dlg.ShowAsync().AsTask();
+
+            simpleDlgButtonOutputBlock.Text = "You clicked: " + button.ToString();
+        }
+    }
+}
