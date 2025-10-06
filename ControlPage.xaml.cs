7d48ddae1a63f61a4a3ff9fb22978ec3c7dbfd8b
SpectralBlade <megajustdancer@gmail.com>
2025-10-06 21:05:49 +0200

    Clean up unneccessary usings

--- ControlPage.xaml.cs
+++ ControlPage.xaml.cs
@@ -1,24 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Security.AccessControl;
 using System.Threading.Tasks;
-using UWPGallery.Controls;
 using UWPGallery.DataModel;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -66,7 +55,7 @@ namespace UWPGallery
             foreach (var uid in Item.RelatedControls)
             {
                 var item = await ControlInfoDataSource.GetItemAsync(uid);
-                
+
                 if (item != null)
                 {
                     col.Add(item);
